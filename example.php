2d1d560909709d27c932c06562bcc574dfb8b490
Jeloo <i.v.yaroslavtsev@gmail.com>
2016-09-12 14:29:00 +0300

    Add GA client id cookie key to Settings, improve the example script

--- example.php
+++ example.php
@@ -1,15 +1,24 @@
 <?php
-
 require_once "vendor/autoload.php";
 
 $httpClient = new \Ivanhoe\SDK\CurlClient();
-$sessionResource = new Ivanhoe\SDK\SessionResource($httpClient);
+$sessionResource = new \Ivanhoe\SDK\SessionResource($httpClient);
 
-$subId = $sessionResource->setCredentials(['test', 'secret'])
-    ->getSubId([
-        'hostname'   => 'http://test.com',
-        'user_agent' => 'Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 950) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Mobile Safari/537.36 Edge/13.10586',
-        'user_ip'    => '127.0.0.1'
-    ]);
+try {
+    $subId = $sessionResource->setCredentials([
+        '11473673099', // developer id
+        'eyJpdiI6IjFPM2ZBOVc5V1FETGV1dE9YblJPaHc9PSIsInZhbHVlIjoiZTREQyt4ZFwvS0VJYXJSV01YWGtDQlNlNzhXWFJnY1JkY3dsVmFWdHhQRDg9IiwibWFjIjoiNGQyODQ2MzEwMmQ5MDdkNzczNTdkYzM5Njc0ZDJmNDY2OGVmYWZkMDg0MmU3YTQ1NjY2NWY1YmIzMDVlZGMxNyJ9'  // password
+    ])
+        ->getSubId([
+            // your website hostname
+            'hostname'         => 'http://test.com',
+            // google analytics profile id
+            // https://developers.google.com/analytics/devguides/collection/analyticsjs/cookies-user-id
+            'google_client_id' => Ivanhoe\SDK\Analytics::getProfileId(),
+        ]);
+} catch (\Ivanhoe\SDK\AnalyticsException $e) {
+    die($e->getMessage());
+}
+
 
 var_dump($subId);
